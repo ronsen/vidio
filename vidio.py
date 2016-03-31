1fea9dd404b6162979ccdef8bc148b5ad3f0764d
Ronald Nababan <ronsen@startrek.or.id>
2016-03-31 16:43:27 +0700

    list of available formats

--- vidio.py
+++ vidio.py
@@ -1,61 +1,95 @@
 #!/usr/bin/env python
+
+import getopt
 import sys
 import urllib2
 import m3u8
 from BeautifulSoup import BeautifulSoup
 import wget
 
-def open_url(url):
+def main(argv):
 	try:
-		opener = urllib2.build_opener(urllib2.HTTPHandler())
-		opener.addheaders = [('User-Agent', 'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)')]
-		request = urllib2.Request(url)
-		page = opener.open(request)
-		html = page.read()
-		return html
+		opts, args = getopt.getopt(argv, "hFf:")
 	except Exception, e:
-		print e
-		sys.exit(1)
+		usage
+		sys.exit()
+
+	url = args[0]
+	fmt = 360
+
+	for opt, arg in opts:
+		if opt == '-h':
+			usage()
+			sys.exit()
+		elif opt == '-F':
+			list_formats(url)
+			sys.exit()
+		elif opt == '-f':
+			fmt = arg
+
+	html = open_url(url)
+	parsed_html = BeautifulSoup(html)
+
+	playlist_url = get_playlist_url(parsed_html)
+	playlist = get_playlist(playlist_url, fmt)
+
+	m3u8_obj = open_playlist(playlist)
+	download_videos(m3u8_obj)
+
+	print "Download finished. You can now manually merge all the downloaded files."
+
+def usage():
+	print 'Usage: vidio.py [VIDIO URL]'
+
+def open_url(url):
+	print '[log] downloading webpage'
+	opener = urllib2.build_opener(urllib2.HTTPHandler())
+	opener.addheaders = [('User-Agent', 'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)')]
+	request = urllib2.Request(url)
+	page = opener.open(request)
+	html = page.read()
+	return html
 
 def get_playlist_url(parsed_html):
-	try:
-		playlists = parsed_html.findAll("div", {"data-clip-hls-url": True})
+	playlists = parsed_html.findAll("div", {"data-clip-hls-url": True})
 
-		for playlist in playlists:
-			return playlist['data-clip-hls-url']
-	except Exception, e:
-		print e
-		sys.exit(1)
+	for playlist in playlists:
+		return playlist['data-clip-hls-url']
 
-def get_playlist(playlist_url, resolution):
+def get_playlist(playlist_url, fmt):
 	m3u8_obj = m3u8.load(playlist_url)
 
 	for playlist in m3u8_obj.playlists:
-		if str(playlist.stream_info).find(resolution) > 0:
+		if str(playlist.stream_info.resolution).find(fmt) > 0:
 			return playlist.uri
+
+	return None
 
 def open_playlist(playlist):
 	m3u8_obj = m3u8.load(playlist)
 	return m3u8_obj
 
 def download_videos(m3u8_obj):
+	print '[log] downloading videos'
 	base_uri = m3u8_obj._base_uri
 
 	for file in m3u8_obj.files:
 		video_url = base_uri + "/" + file
 		wget.download(video_url)
 
-if __name__ == "__main__":
-	url = sys.argv[1]
-
+def list_formats(url):
 	html = open_url(url)
 	parsed_html = BeautifulSoup(html)
 
 	playlist_url = get_playlist_url(parsed_html)
-	playlist = get_playlist(playlist_url, "360")
+	m3u8_obj = m3u8.load(playlist_url)
 
-	m3u8_obj = open_playlist(playlist)
-	download_videos(m3u8_obj)
+	for playlist in m3u8_obj.playlists:
+		print playlist.stream_info.resolution
 
-	print "Download finished. You can now manually merge all the downloaded files."
-	
+if __name__ == "__main__":
+	try:
+		main(sys.argv[1:])
+	except Exception, e:
+		print e
+		sys.exit()
